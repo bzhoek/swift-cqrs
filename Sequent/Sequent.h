3df4ee9be515e07a138375d4cf3f22d761019d5d
Bas van der Hoek <bvdhoek@zilverline.com>
2016-08-30 13:34:55 +0200

    Initial Commit

---
+++ Sequent/Sequent.h
@@ -0,0 +1,19 @@
+//
+//  Sequent.h
+//  Sequent
+//
+//  Created by Bas van der Hoek on 30/08/16.
+//  Copyright © 2016 Bas van der Hoek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sequent.
+FOUNDATION_EXPORT double SequentVersionNumber;
+
+//! Project version string for Sequent.
+FOUNDATION_EXPORT const unsigned char SequentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sequent/PublicHeader.h>
+
+
