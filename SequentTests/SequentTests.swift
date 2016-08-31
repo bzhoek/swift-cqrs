e12260b930fa4e645d4cfbf9c0f8a4c0550b7064
Bas van der Hoek <bvdhoek@zilverline.com>
2016-08-31 18:14:51 +0200

    command bus work in progress

--- SequentTests/SequentTests.swift
+++ SequentTests/SequentTests.swift
@@ -1,36 +1,89 @@
-//
-//  SequentTests.swift
-//  SequentTests
-//
-//  Created by Bas van der Hoek on 30/08/16.
-//  Copyright © 2016 Bas van der Hoek. All rights reserved.
-//
-
 import XCTest
 @testable import Sequent
 
+class Command {
+  let uuid: NSUUID
+  init(uuid: NSUUID) {
+    self.uuid = uuid
+  }
+}
+
+class CommandBus {
+  let handlers: [CommandHandler]
+
+  init(handlers: [CommandHandler]) {
+    self.handlers = handlers
+  }
+
+  func dispatch(command: Command) {
+    handlers.map {
+      $0.handle(command)
+    }.flatMap {
+      $0
+    }.flatMap {
+      self.emit($0)
+    }
+  }
+
+  func emit(events: [Event]) {
+
+  }
+}
+
+class CommandHandler {
+  func handle(command: Command) -> [Event]? {
+    return .None
+  }
+}
+
+class CommandBusTests: XCTestCase {
+
+  class MockCommandHandler: CommandHandler {
+    override func handle(command: Command) -> [Event]? {
+      return [Event()]
+    }
+  }
+
+  func testCommandBus() {
+    let bus = CommandBus(handlers: [MockCommandHandler()])
+    let command = Command(uuid: NSUUID(UUIDString: "01000000-0000-0000-0000-000000000000")!)
+    bus.dispatch(command)
+  }
+}
+
+class CreateTodo: Command {
+  let title: String
+  let description: String
+
+  init(uuid: NSUUID, title: String, description: String) {
+    self.title = title
+    self.description = description
+    super.init(uuid: uuid)
+  }
+}
+
+class Aggregate {
+}
+
+class Event {
+}
+
+class EventStore {
+}
+
 class SequentTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+
+  override func setUp() {
+    super.setUp()
+  }
+
+  override func tearDown() {
+    super.tearDown()
+  }
+
+  func testCommandUUID() {
+    let command = Command(uuid: NSUUID(UUIDString: "01000000-0000-0000-0000-000000000000")!)
+    XCTAssertEqual("01000000-0000-0000-0000-000000000000", command.uuid.UUIDString)
+  }
+
 }
